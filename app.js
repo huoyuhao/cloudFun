6a87a75e17e554122ba5397a40a564ea35169e7e
liamhuo <liamhuo@tencent.com>
2024-08-05 16:03:54 +0800

    修改 express

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // 解析请求入参
-app.use(express.json());
+// app.use(express.json());
 
 app.use('/api/*', async (req, res, next) => {
   // res.setHeader('Access-Control-Allow-Origin', '*');
