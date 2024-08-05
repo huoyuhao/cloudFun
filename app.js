e9714af7cf2d12856c33d4127a7b809e8d08cf55
liamhuo <liamhuo@tencent.com>
2024-08-05 15:45:14 +0800

    修改 请求内容

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-const express = require('express');
+import express from 'express';
+// import routes from './router/index';
+
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+// 请求主体大小限制
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// 解析请求入参
+app.use(express.json());
+
+app.use('/api/*', async (req, res, next) => {
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  res.setHeader('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.get('/user', (req, res) => {
-  res.send([
-    {
-      title: 'serverless framework',
-      link: 'https://serverless.com'
-    }
-  ]);
-});
-
-app.get('/user/:id', (req, res) => {
-  const { id } = req.params;
-  res.send({
-    id,
-    title: 'serverless framework',
-    link: 'https://serverless.com'
-  });
-});
-
-app.get('/404', (req, res) => {
-  res.status(404).send('Not found');
-});
-
-app.get('/500', (req, res) => {
-  res.status(500).send('Server Error');
+// checkHealth
+app.get('/checkHealth', (req, res) => {
+  res.json({ code: 0, data: '接口检测正常!' });
 });
 
 // Error handler
@@ -39,6 +34,9 @@ app.use((err, req, res) => {
   res.status(500).send('Internal Serverless Error');
 });
 
+// api请求路由
+// routes(app);
+
 app.listen(9000, () => {
-  console.log('Server start on http://localhost:9000');
+  console.log(`App started on port ${9000}`);
 });
