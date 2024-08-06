fdf2f703be202fb6c0fb36e9123f7fd378d58d29
liamhuo <liamhuo@tencent.com>
2024-08-06 11:25:54 +0800

    修改

--- router/menu.js
+++ router/menu.js
@@ -1,27 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const connection = await mysql.createConnection({
+  host: '172.16.0.15',
+  user: 'root',
+  database: 'menu',
+  password: '366351&huo'
+});
 
-const DB_HOST = '172.16.0.15';
-const DB_PORT = 3306;
-const DB_DATABASE = 'menu';
-const DB_USER = 'root';
-const DB_PASSWORD = '366351&huo';
-// 创建连接池
-const { database } = require('scf-nodejs-serverlessdb-sdk');
-
-const mainHandler = async () => {
-  // use connection
-  const connection = await database('menu').connection();
-  const result = await connection.queryAsync('select * from users');
-  connection.release(); // must release before return
-  console.log('db2 query result:', result);
-};
+connection.query(
+  'SELECT * FROM `users`',
+  (err, results, fields) => {
+    console.log(11, results); // 结果集
+    console.log(22, fields); // 额外的元数据（如果有的话）
+  }
+);
 // 菜单服务
 router.post('/', async (req, res) => {
   const data = req.body;
   console.info(data);
-  mainHandler();
   const result = { code: 0, data };
   res.json(result);
 });
