4963832a60c9ac2214946aaf90c82747b1dac192
liamhuo <liamhuo@tencent.com>
2024-08-06 14:48:58 +0800

    报错信息

--- router/menu.js
+++ router/menu.js
@@ -2,20 +2,29 @@ const express = require('express');
 const mysql = require('mysql2');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const connection = await mysql.createConnection({
-  host: '172.16.0.15',
-  user: 'root',
-  database: 'menu',
-  password: '366351&huo'
-});
 
-connection.query(
-  'SELECT * FROM `users`',
-  (err, results, fields) => {
-    console.log(11, results); // 结果集
-    console.log(22, fields); // 额外的元数据（如果有的话）
+const fun = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: '172.16.0.15',
+      user: 'root',
+      port: 3306,
+      database: 'menu',
+      password: '366351&huo'
+    });
+
+    connection.query(
+      'SELECT * FROM `users`',
+      (err, results) => {
+        console.log(0, err); // 报错
+        console.log(11, results); // 结果集
+      }
+    );
+  } catch (e) {
+    console.log(e);
   }
-);
+}
+fun();
 // 菜单服务
 router.post('/', async (req, res) => {
   const data = req.body;
