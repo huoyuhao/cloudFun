fb3932a51d346fa18dd7a53ee632b3da2bbd5b9d
liamhuo <liamhuo@tencent.com>
2024-08-06 11:05:28 +0800

    数据库

--- router/menu.js
+++ router/menu.js
@@ -1,18 +1,39 @@
 const express = require('express');
-const db = require('../utils/mysql');
-
+const mysql = require('mysql2');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+
+const DB_HOST = '172.16.0.15';
+const DB_PORT = 3306;
+const DB_DATABASE = 'menu';
+const DB_USER = 'root';
+const DB_PASSWORD = '366351&huo';
+// 创建连接池
+const pool = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  connectionLimit: 1
+});
 // 菜单服务
 router.post('/', async (req, res) => {
   const data = req.body;
   console.info(data);
+  pool.getConnection((err, connection) => {
+    if (err) return err;
+    connection.query('select * from users where id = ?', [1], (error, results) => {
+      if (error) {
+        return error;
+      }
+         res.json({ code: 200, data: results[0], msg: '0k' }); // 成功返回的数据
 
-  const sql = 'SELECT *  FROM users;';
-  const list = await db.query(sql, '');
-  console.info(list);
-
-  const result = { code: 0, data: list };
-  res.json(result);
+      // 将连接返回到连接池中, 准备让其他人重复使用
+      connection.release();
+    });
+  });
+  // const result = { code: 0, data: list };
+  // res.json(result);
 });
 module.exports = router;
--- utils/mysql.js
+++ utils/mysql.js
@@ -1,40 +1,40 @@
-'use strict';
-const DB_HOST = '172.16.0.15';
-const DB_PORT = 3306;
-const DB_DATABASE = 'menu';
-const DB_USER = 'root';
-const DB_PASSWORD = '366351&huo';
-const pool = require('mysql2').createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  port: DB_PORT,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  connectionLimit: 1
-});
+// 'use strict';
+// const DB_HOST = '172.16.0.15';
+// const DB_PORT = 3306;
+// const DB_DATABASE = 'menu';
+// const DB_USER = 'root';
+// const DB_PASSWORD = '366351&huo';
+// const pool = require('mysql2').createPool({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   port: DB_PORT,
+//   password: DB_PASSWORD,
+//   database: DB_DATABASE,
+//   connectionLimit: 1
+// });
 
-// 获取数据库连接的函数
-const connect = () => {
-  return new Promise((resolve, reject) => {
-      pool.getConnection((err, connection) => {
-          // 从连接池中获取连接
-          !err ? resolve(connection) : reject(err); // 若成功获取连接，返回连接对象，否则返回错误
-      });
-  });
-};
+// // 获取数据库连接的函数
+// const connect = () => {
+//   return new Promise((resolve, reject) => {
+//       pool.getConnection((err, connection) => {
+//           // 从连接池中获取连接
+//           !err ? resolve(connection) : reject(err); // 若成功获取连接，返回连接对象，否则返回错误
+//       });
+//   });
+// };
 
-// 需要传入sql语句和参数
-const query = (sql, params) => {
-  return new Promise((resolve, reject) => {
-     // 获取数据库连接
-      connect().then((connection) => {
-        connection.query(sql, params, (err, results) => {
-            // 执行 SQL 查询语句
-            !err ? resolve(results) : reject(err); // 若查询成功，返回查询结果，否则返回错误
-             connection.release(); // 释放连接
-        });
-      });
-  });
-};
-// 导出 connect 和 query 函数
-module.exports = { connect, query };
+// // 需要传入sql语句和参数
+// const query = (sql, params) => {
+//   return new Promise((resolve, reject) => {
+//      // 获取数据库连接
+//       connect().then((connection) => {
+//         connection.query(sql, params, (err, results) => {
+//             // 执行 SQL 查询语句
+//             !err ? resolve(results) : reject(err); // 若查询成功，返回查询结果，否则返回错误
+//              connection.release(); // 释放连接
+//         });
+//       });
+//   });
+// };
+// // 导出 connect 和 query 函数
+// module.exports = { connect, query };
