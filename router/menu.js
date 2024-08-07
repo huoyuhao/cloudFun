2847c6827cb0ea4c0eab2ee4c2d508416d1ecb33
liamhuo <liamhuo@tencent.com>
2024-08-07 16:20:04 +0800

    数据库

--- router/menu.js
+++ router/menu.js
@@ -1,52 +1,15 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const { menuQuery } = require('../utils/mysql');
 
-// const database = require('scf-nodejs-serverlessdb-sdk').database
- 
-// const fun = async () => {
-//   //use connection
-//   console.log('start connection mysql')
-//   const connection = await database('menu').connection()
-//   const result = await connection.queryAsync('select * from users')
-//   connection.release() //must release before return
-//   console.log('db2 query result:',result)
-// }
-
-
-
-const mysql = require('mysql')
-
-const fun = async () => {
-  try {
-    const connection =  mysql.createConnection({
-      host: '172.16.0.15',
-      user: 'root',
-      port: 3306,
-      database: 'menu',
-      password: '366351&huo'
-    })
-    connection.connect();
-
-    connection.query(
-      'SELECT * FROM `users`',
-      (err, results) => {
-        console.log(0, err); // 报错
-        console.log(11, results); // 结果集
-      }
-    );
-  } catch (e) {
-    console.log(e);
-  }
-}
 // 菜单服务
 router.post('/', async (req, res) => {
   const data = req.body;
   console.info(data);
-  fun();
+  menuQuery('SELECT * FROM `users`');
   const result = { code: 0, data };
   res.json(result);
 });
-
 
 module.exports = router;
