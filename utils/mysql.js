84c24d43732464f863666704129f18cc70ef99af
liamhuo <liamhuo@tencent.com>
2024-08-08 16:23:22 +0800

    查看查询报错

--- utils/mysql.js
+++ utils/mysql.js
@@ -26,19 +26,61 @@ const getConnection = (pool) => { // 單獨一個connect才可以用rollback
     });
   });
 };
+
+const beginTransaction = (connection) => { // 開啟一個新交易事件
+  return new Promise((resolve, reject) => {
+    connection.beginTransaction((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+};
+const commitTransaction = (connection) => { // 完整的提交
+  return new Promise((resolve, reject) => {
+    connection.commit((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+};
+
+const rollbackTransaction = (connection) => { // 失誤的話要滾動返回
+  return new Promise((resolve) => {
+    connection.rollback(() => {
+      resolve();
+    });
+  });
+};
+const queryConnection = (connection, query) => { // 增加參數 connection的調用，後面方便使用query來寫SQL語句
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
 // 封装单独sql执行函数 每次执行是一个单独的连接池连接 执行完成释放
 const executeQuery = (pool, sql) => {
   return new Promise((resolve, reject) => {
-    getConnection(pool).then((connection) => {
-      connection.query(sql, (queryErr, results) => {
-        connection.release();
-        if (queryErr) {
-          reject(queryErr);
-        } else {
-          resolve(results);
-        }
+    try {
+      getConnection(pool).then((connection) => {
+        connection.query(sql, (queryErr, results) => {
+          connection.release();
+          if (queryErr) {
+            console.error(301, queryErr);
+            reject(queryErr);
+          } else {
+            resolve(results);
+          }
+        });
       });
-    });
+    } catch (err) {
+        console.error(303, err);
+        reject(err);
+      }
   });
 };
 
@@ -48,5 +90,14 @@ const menuQuery = (sql) => {
 const marriageQuery = (sql) => {
   return executeQuery(marriagePool, sql);
 };
+const getMenuTransaction = () => {
+  return {
+    getConnection,
+    beginTransaction,
+    queryConnection,
+    commitTransaction,
+    rollbackTransaction,
+  };
+};
 
-module.exports = { menuQuery, marriageQuery };
+module.exports = { menuQuery, marriageQuery, getMenuTransaction };
