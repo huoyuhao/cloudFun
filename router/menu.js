05714f33ff8cf75fba2a989b3c0bcfd11bfdcc0d
liamhuo <liamhuo@tencent.com>
2024-08-14 18:44:17 +0800

    优化

--- router/menu.js
+++ router/menu.js
@@ -33,15 +33,29 @@ router.get('/list', async (req, res) => {
       menuQuery(`select * from menu_tag where menu_id = ${id}`),
       menuQuery(`select * from menu_step where menu_id = ${id}`),
     ]);
-    data = { ...menu,  materials, images, tags, steps }
+    data = { ...menu[0],  materials, images, tags, steps }
   } else {
-    const res = await Promise.all([
+    const [menus, ...list] = await Promise.all([
       menuQuery(`select * from menu`),
       menuQuery(`select * from menu_material`),
       menuQuery(`select * from menu_image`),
       menuQuery(`select * from menu_tag`),
       menuQuery(`select * from menu_step`),
     ]);
+    const tranObj = { 1: 'materials', 2: 'images', 3: 'tags', 4: 'steps'}
+    const obj = {};
+    list.forEach((arr, index) => {
+      arr.forEach((item) => {
+        if (!obj[item.menu_id]) obj[item.menu_id] = {}
+        if (!obj[item.menu_id][tranObj[index]]) obj[item.menu_id][tranObj[index]] = []
+        obj[item.menu_id][tranObj[index]].push(item)
+      });
+    })
+    data = menus.map(item => {
+      const { materials, images, tags, steps } = obj[item.id];
+      return { ...item, materials, images, tags, steps };
+    })
+    console.log(obj);
   }
   const result = { code: 0, data };
   res.json(result);
