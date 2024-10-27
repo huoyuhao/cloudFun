ef41693dc6230fbf2501765d90d52bdf84a2cab4
liamhuo <liamhuo@tencent.com>
2024-10-27 18:58:05 +0800

    axios请求

--- router/user.js
+++ router/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const config = require('config');
-const axios = require('axios');
+import axios from 'axios';
 const { getUrlParam } = require('../utils/common.js');
 
 // eslint-disable-next-line new-cap
@@ -23,21 +23,7 @@ router.get('/login', async (req, res) => {
       grant_type: 'authorization_code',
     };
     const str = getUrlParam(postData);
-    try {
-      
-      axios
-      .get(`${api}${str}`)
-      .then((res) => {
-        console.log(111, res);
-        resolve({ code: -1, data: res });
-      })
-      .catch((error) => {
-        resolve(error);
-      });
-    } catch (e) {
-      console.log('e', e);
-      resolve(e);
-    }
+    resolve({ code: -1, data: str });
   });
   res.json(result);
 });
