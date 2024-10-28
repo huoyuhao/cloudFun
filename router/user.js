a42abaa5b07845f963589e3545ffc65b8baa2b78
liamhuo <liamhuo@tencent.com>
2024-10-28 15:22:16 +0800

    插入数据

--- router/user.js
+++ router/user.js
@@ -25,28 +25,27 @@ router.get('/login', async (req, res) => {
       grant_type: 'authorization_code',
     };
     const str = getUrlParam(postData);
-    request.get(`${api}${str}`)
-      .then(async (res) => {
-        const { openid } = res;
-        // 生成token 用户后续请求验证实效性
+    request.get(`${api}${str}`).then(async (res) => {
+      const { openid } = res;
+      // 查询用户是否存在
+      const result = await menuDb.select('*').from('user').where('openid', openid, 'eq').queryRow();
+      if (!result) {
         const trans = await menuDb.useTransaction();
         try {
-          const result = await menuDb
-          .select('*')
-          .from('user')
-          .where('openid', openid, 'eq')
-          .queryRow();
-          console.log(111, result);
+          // 存储用户信息
+          await trans.insert("user").column("openid", openid);
+          await trans.insert("user").column("name", openid);
+          await trans.commit();
+          resolve({ code: 0, data: openid });
         } catch (err) {
           await trans.rollback();
-          res.json({ code: 300, msg: '用户创建失败', data: null });
+          throw '用户注册失败';
         }
-        resolve({ code: 0, data: openid });
-      })
-      .catch((error) => {
-        resolve({ code: -1, msg: '获取用户信息失败', error });
-      });
-    // 查询用户是否存在
+      }
+    })
+    .catch((error) => {
+      resolve({ code: -1, msg: '用户登录失败', error });
+    });
     // 判断是否注册 如果没有存储用户信息 后期更新用户头像、用户昵称
   });
   res.json(result);
--- utils/menu.sql
+++ utils/menu.sql
@@ -2,28 +2,12 @@ CREATE TABLE `menu` (
   `id` INT(4) PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL COMMENT '菜单名称',
   `condiment` varchar(100) DEFAULT NULL COMMENT '调料',
+  `tag` varchar(200) DEFAULT NULL COMMENT '分类',
+  `material` varchar(200) DEFAULT NULL COMMENT '材料',
+  `step` varchar(2000) DEFAULT NULL COMMENT '步骤',
   `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单表';
-
-CREATE TABLE `menu_tag` (
-  `id` INT(4) PRIMARY KEY AUTO_INCREMENT,
-  `menu_id` INT(4) DEFAULT NULL COMMENT '菜单ID',
-  `content` varchar(100) DEFAULT NULL COMMENT 'Tag名称',
-  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单标签表';
-
-
-CREATE TABLE `menu_step` (
-  `id` INT(4) PRIMARY KEY AUTO_INCREMENT,
-  `menu_id` INT(4) DEFAULT NULL COMMENT '菜单ID',
-  `content` varchar(500) DEFAULT NULL COMMENT '步骤内容',
-  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单步骤表';
-
-
 
 CREATE TABLE `menu_image` (
   `id` INT(4) PRIMARY KEY AUTO_INCREMENT,
@@ -34,12 +18,14 @@ CREATE TABLE `menu_image` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单图片表';
 
 
-
-CREATE TABLE `menu_material` (
+CREATE TABLE `user` (
   `id` INT(4) PRIMARY KEY AUTO_INCREMENT,
-  `menu_id` INT(4) DEFAULT NULL COMMENT '菜单ID',
-  `content` varchar(300) DEFAULT NULL COMMENT '菜单原料',
-  `number` varchar(10) DEFAULT NULL COMMENT '菜单原料数量',
+  `openid` INT(4) DEFAULT NULL COMMENT 'Openid',
+  `name` varchar(20) DEFAULT NULL COMMENT '用户昵称',
+  `user_img` varchar(100) DEFAULT NULL COMMENT '用户头像',
+  `sex` TINYINT(1) DEFAULT NULL COMMENT '性别',
+  `iphone` INT(20) DEFAULT NULL COMMENT '电话',
+  `bind_openid` varchar(20) DEFAULT NULL COMMENT '绑定用户openid',
   `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `modified_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单原料表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
