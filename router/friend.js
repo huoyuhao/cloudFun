f14e96fd7cc1b9cd614cb059edca16465ea6d878
liamhuo <liamhuo@tencent.com>
2024-11-26 11:22:10 +0800

    个人信息

--- router/friend.js
+++ router/friend.js
@@ -53,16 +53,22 @@ router.get('/detail', async (req, res) => {
       .column('operate_type', '浏览')
       .execute();
     // 获取用户是否被收藏
-    const collectInfo = await menuDb
-      .select('*')
-      .from('friend_browse')
-      .where('openid', openid)
-      .where('user_id', userInfo.id)
-      .where('operate_user_id', id)
-      .where('operate_type', '收藏')
-      .queryRow();
-    userItem.collect = collectInfo;
-    res.json({ code: 0, data: transData(userItem) });
+    try {
+      const collectInfo = await menuDb
+        .select('*')
+        .from('friend_browse')
+        .where('openid', openid)
+        .where('user_id', userInfo.id)
+        .where('operate_user_id', id)
+        .where('operate_type', '收藏')
+        .queryRow();
+      console.log('collectInfo', collectInfo);
+      userItem.collect = collectInfo;
+      res.json({ code: 0, data: transData(userItem) });
+    } catch (err) {
+      console.log('err', err);
+      res.json({ code: 0, data: transData(userItem) });
+    }
   }
   res.json({ code: 0, data: transData(userInfo) });
 });
@@ -74,9 +80,8 @@ router.post('/index', async (req, res) => {
     return res.json({ code: 200, msg: '未登录', data: null });
   }
   const userItem = await menuDb.select('id').from('friend_user').where('openid', openid).queryRow();
-  console.log('userItem', userItem);
   if (!userItem) {
-    // 用户信息为空 插入一条数据 然后更新传入字段
+    // 用户信息为空 先插入数据 然后更新传入字段
     await menuDb.insert('friend_user').column('openid', openid).execute();
   }
   const updateObj = {};
@@ -85,13 +90,16 @@ router.post('/index', async (req, res) => {
       updateObj[key] = data[key];
     }
   });
-  console.log('updateObj', updateObj);
-  const result = await menuDb
-    .update('friend_user', updateObj)
-    .where('openid', openid)
-    .execute();
-  // 获取更新数据 进行更新
-  res.json({ code: 0, data: result });
+  try {
+    const result = await menuDb
+      .update('friend_user', updateObj)
+      .where('openid', openid)
+      .execute();
+    // 获取更新数据 进行更新
+    res.json({ code: 0, data: result });
+  } catch (err) {
+    res.json({ code: 300, msg: '更新失败', data: null });
+  }
 });
 
 // 收藏
@@ -109,15 +117,52 @@ router.post('/collect', async (req, res) => {
   if (userInfo.id !== id) {
     // 判断是否已经收藏过了 如果已经收藏 则取消收藏
     // 插入用户访问详情记录
-    menuDb.insert('friend_browse')
-      .column('openid', openid)
-      .column('user_id', userInfo.id)
-      .column('operate_user_id', id)
-      .column('operate_type', '收藏')
-      .execute();
-    res.json({ code: 0, data: null });
+    try {
+      menuDb.insert('friend_browse')
+        .column('openid', openid)
+        .column('user_id', userInfo.id)
+        .column('operate_user_id', id)
+        .column('operate_type', '收藏')
+        .execute();
+      res.json({ code: 0, data: null });
+    } catch (err) {
+      res.json({ code: 300, msg: '收藏失败', data: null });
+    }
   }
   res.json({ code: 0, data: null });
+});
+
+// 获取个人中心详情
+router.get('/user/info', async (req, res) => {
+  const openid = req.headers['x-user-openid'];
+  if (!openid) {
+    return res.json({ code: 200, msg: '未登录', data: null });
+  }
+  const userInfo = await menuDb.select('id, name, user_img').from('friend_user').where('openid', openid).queryRow();
+  // 看过我的
+  userInfo.browseNum = await menuDb
+    .select('count(1)')
+    .from('friend_browse')
+    .where('operate_user_id', userInfo.id)
+    .where('operate_type', '浏览')
+    .queryValue();
+    // 我被收藏的数量
+    userInfo.collectedNum = await menuDb
+    .select('count(1)')
+    .from('friend_browse')
+    .where('operate_user_id', userInfo.id)
+    .where('operate_type', '收藏')
+    .queryValue();
+    // 我收藏别人的数量
+    userInfo.collectNum = await menuDb
+      .select('count(1)')
+      .from('friend_browse')
+      .where('user_id', userInfo.id)
+      .where('operate_type', '收藏')
+      .queryValue();
+      userInfo;
+  // 如果没有id 返回个人信息，有id 返回用户详情
+  res.json({ code: 0, data: transData(userInfo) });
 });
 
 module.exports = router;
