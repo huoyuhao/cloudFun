d1a20e720420ed2f53536667508ca1333b405ecb
liamhuo <liamhuo@tencent.com>
2024-11-26 16:17:16 +0800

    删除

--- task/friend.js
+++ task/friend.js
@@ -2,10 +2,6 @@ const schedule = require('node-schedule');
 const { menuDb } = require('../utils/ali-mysql');
 
 const initScheduleTask = () => {
-  menuDb.insert('friend_browse')
-    .column('openid', 'test')
-    .column('operate_number', 3)
-    .execute();
   // 每天凌晨3点30分30秒触发定时任务
   schedule.scheduleJob('10 30 * * * *', () => {
     console.log(`${new Date()} 定时任务开始。`);
