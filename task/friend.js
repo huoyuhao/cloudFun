1bb6f69e0004683af0f324a56000e5d481c16519
liamhuo <liamhuo@tencent.com>
2024-11-26 16:05:04 +0800

    更新实际

--- task/friend.js
+++ task/friend.js
@@ -5,10 +5,13 @@ const initScheduleTask = () => {
   // 每天凌晨3点30分30秒触发定时任务
   schedule.scheduleJob('* 10 * * * *', () => {
     console.log(`${new Date()} 定时任务开始。`);
-    menuDb.insert('friend_browse')
-      .column('openid', 'test')
-      .column('operate_number', 1)
-      .execute();
+    const dateTime = dayjs().format('HH:mm:ss')
+    if (dateTime === '16:10:00') {
+      menuDb.insert('friend_browse')
+        .column('openid', 'test')
+        .column('operate_number', 1)
+        .execute();
+    }
   });
 };
 
