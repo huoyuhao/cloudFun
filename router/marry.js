7c8edbff71400a8763a81326e0278e0717541ffe
huoyuhao <huoyuhao@bytedance.com>
2025-04-06 22:34:51 +0800

    desc关键字 删除 修改为 info

--- router/marry.js
+++ router/marry.js
@@ -7,7 +7,7 @@ const { transData } = require('../utils/common.js');
 router.get('/list', async (req, res) => {
   // 查询条件
   const userItem = await menuDb
-    .select('name, desc').from('marry_user')
+    .select('name, info').from('marry_user')
     .queryList();
   if (userItem) {
     res.json({ code: 0, data: transData(userItem) });
@@ -31,7 +31,7 @@ router.get('/detail', async (req, res) => {
 // 新增
 router.post('/index', async (req, res) => {
   const data = req.body || {};
-  const { name, tel, num, desc } = data;
+  const { name, tel, num, info } = data;
   const userItem = await menuDb.select('id').from('marry_user').where('name', name).queryRow();
   if (!userItem) {
     // 用户信息为空 先插入数据 然后更新传入字段
@@ -39,14 +39,14 @@ router.post('/index', async (req, res) => {
       .column('name', name)
       .column('tel', tel)
       .column('num', num)
-      .column('desc', desc)
+      .column('info', info)
       .execute();
     return res.json({ code: 0, data: '登记成功' });
   }
   await menuDb.update('marry_user')
     .column('tel', tel)
     .column('num', num)
-    .column('desc', desc)
+    .column('info', info)
     .where('id', userItem.id)
     .execute();
     res.json({ code: 0, data: '更新登记成功' });
